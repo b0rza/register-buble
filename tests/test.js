e956cc763c0da2d697c701103724250a01c25263
Ante Borzić <aborzic@extensionengine.com>
2016-09-19 13:16:58 +0200

    Outline error tests

--- tests/test.js
+++ tests/test.js
@@ -11,3 +11,33 @@ test('test require hook', t => {
   t.equals(result.data.meaningOfLife, 42);
   t.end();
 });
+
+test('test compile errors', t => {
+  require('../register.js');
+  let unsupported_path = '../example/unsupported.js';
+
+  try {
+    require(unsupported_path);
+  } catch (err){
+    t.comment(err);
+    t.equal(err.name, 'CompileError');
+  }
+
+  t.end();
+});
+
+
+test('test module not found errors', t => {
+  require('../register.js');
+  let not_found_path = '../example/not_found.js';
+
+  try {
+    require(not_found_path);
+  }
+  catch (err){
+    t.equal(err.code, 'MODULE_NOT_FOUND');
+    t.equal(err.message, `Cannot find module '${not_found_path}'`);
+  }
+
+  t.end();
+});
